b55d52f49dc238f5b6336642a3a06d96ef6a65b2
simdhlu022 <simdhlu022@student.wethinkcode.co.za>
2023-02-26 16:52:42 +0200

    changes

--- manager/manager.py
+++ manager/manager.py
@@ -1,5 +1,8 @@
 import typer
 from enum import Enum
+from datetime import date, timedelta
+
+from authenticate_user import authenticate_user, valid_credentials
 
 app = typer.Typer()
 
@@ -13,15 +16,16 @@ def login():
     '''
     :   authenticate user
     '''
-    pass
+    creds = valid_credentials()
+    return authenticate_user(creds)
 
 
-@app.command()
-def register():
-    '''
-    :   register a new user
-    '''
-    pass
+# @app.command()
+# def register():
+#     '''
+#     :   register a new user
+#     '''
+#     pass
 
 
 @app.command()
@@ -29,7 +33,18 @@ def add():
     '''
     :   add a new task
     '''
-    pass
+    task_complete = "No"
+    task_user = input("Please enter the username who has been assigned the task: ")
+    task_title = input("Please enter the name of the task: ")
+    task_descrip = input("Please enter a description of the task:\n")
+    task_assigned = date.today()
+    task_due = task_assigned + timedelta(days=2)
+
+    new_task = (f"\n {task_user}, {task_title}, {task_descrip}, {str(task_assigned)}, {str(task_due)}, {task_complete}")
+
+    with open('tasks.txt', 'a') as g:
+        g.write(new_task)
+    print("\nNew task successfully added!\n")
 
 
 def view(choice: ViewChoice):
@@ -37,9 +52,35 @@ def view(choice: ViewChoice):
     :   view assigned tasks
     '''
     if view.value == 'all':
-        pass
+        with open('tasks.txt', 'r') as f:
+            for line in f:
+                print(f"Task             :   {line.split(', ')[1]}")
+                print(f"Assigned to      :   {line.split(', ')[0]}")
+                print(f"Date Assigned    :   {line.split(', ')[3]}")
+                print(f"Due Date         :   {line.split(', ')[4]}")
+                print(f"Task Completed   :   {line.split(', ')[5]}")
+                print(f"Task Description :   {line.split(', ')[2]}")
+                print('\n')
     if view.value == 'mine':
-        pass
+        username = input("Enter your username: ")
+        g = open('temp.txt', 'w+')
+        with open('tasks.txt', 'r') as f:
+
+            count = 1
+            tasks = []
+            for line in f:
+
+                if line.startswith(username):
+                    print(f"Task             :   {line.split(', ')[1]}")
+                    print(f"Assigned to      :   {line.split(', ')[0]}")
+                    print(f"Date Assigned    :   {line.split(', ')[3]}")
+                    print(f"Due Date         :   {line.split(', ')[4]}")
+                    print(f"Task Completed   :   {line.split(', ')[5]}")
+                    print(f"Task Description :   {line.split(', ')[2]}")
+                    print('\n')
+                tasks.append(line)
+                count += 1
+                g.write(str(tasks))
 
 
 @app.command()
@@ -53,7 +94,7 @@ def reports():
 @app.command()
 def stats():
     '''
-    display task statistics
+    :   display task statistics
     '''
     pass
 
