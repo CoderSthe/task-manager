55ac6e756df0ccbb5fe9ec979004f8fd4c6e13a4
simdhlu022 <simdhlu022@student.wethinkcode.co.za>
2023-02-26 19:34:27 +0200

    new files added

---
+++ manager/disp_stats.py
@@ -0,0 +1,27 @@
+import sys
+def display_stats():
+    with open('current_cred.txt', 'r') as f:
+        username = f.read()
+    if username != "admin":
+        print("You have selected an invalid option. Please try again\n")
+        sys.exit()
+    else:
+        f = open('user.txt', 'r')
+        g = open('tasks.txt', 'r')
+
+        num_users = 0
+        num_tasks = 0
+
+        for line in f:
+            num_users += 1
+
+        for line in g:
+            num_tasks += 1
+
+        print(f"Number of users             :   {num_users}")
+        print(f"Number of tasks             :   {num_tasks}")
+
+        # close the files
+            
+        f.close()
+        g.close()
---
+++ manager/gen_reports.py
@@ -0,0 +1,36 @@
+
+import sys
+def generate_reports():
+    with open('current_cred.txt', 'r') as f:
+        username = f.read()
+    if username != "admin":
+        print("This is an invalid option.\n")
+        sys.exit()
+    else:
+        to = open('task_overview.txt', 'w')
+        uo = open('user_overview.txt', 'w')
+        f1 = open('tasks.txt', 'r')
+        f2 = open('user.txt', 'r')
+
+        task_count = 0
+        user_count = 0
+
+        for i in f1:
+            task_count += 1
+
+        to.write("Total number of tasks generated:\t" + str(task_count) + '\n')
+        print("task_overview.txt report succcessfully generated.")
+
+        for i in f2:
+            user_count += 1
+
+        uo.write("Total number of users registered:\t" + str(user_count) + '\n')
+        uo.write("Total number of tasks generated:\t" + str(task_count) + '\n')
+
+
+        print("useroverview.txt report successfully generated.")
+
+        to.close()
+        uo.close()
+        f1.close()
+        f2.close()
