fd0fb841dd6e8f7812a66ea4492ffae0e3143ac3
simdhlu022 <simdhlu022@student.wethinkcode.co.za>
2023-02-27 14:47:52 +0200

    minor changes to text format

--- manager/registration.py
+++ manager/registration.py
@@ -2,11 +2,9 @@ import hashlib
 import getpass
 
 def register_user():
-    email = input("Enter email address: ")
+    email = input("Enter email address: ").lower()
     password = getpass.getpass("Enter password: ")
     confirmation = getpass.getpass("Confirm password: ")
-    # password = input("Enter password: ")
-    # confirmation = input("Confirm password: ")
 
     if confirmation == password:
         enc = confirmation.encode()
@@ -14,7 +12,6 @@ def register_user():
 
         with open("credentials.txt", "a") as f:
             f.write(f"{email}, {hash1}\n")
-            # f.write(f"{hash1}\n")
         print("You have successfully been registered!")
     else:
         print("Password and password confirmation do not match.\n")
