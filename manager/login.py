be2e80fc1491e14cc698b9eeefa6d4e57d99bc9e
simdhlu022 <simdhlu022@student.wethinkcode.co.za>
2023-02-27 11:30:48 +0200

    new login script using hashlib

---
+++ manager/login.py
@@ -0,0 +1,15 @@
+import hashlib
+
+def login_user():
+    email = input("Enter email: ")
+    password = input("Enter password: ")
+
+    auth = password.encode()
+    auth_hash = hashlib.md5(auth).hexdigest()
+    with open("credentials.txt", "r") as f:
+        stored_email, stored_password = f.read().split("\n")
+    
+    if email == stored_email and auth_hash == stored_password:
+        print("Logged in successfully!")
+    else:
+        print("Login failed!\n")
