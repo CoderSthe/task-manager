3f7263c7d6f6369b50f16228df1f4597c1f518fb
simdhlu022 <simdhlu022@student.wethinkcode.co.za>
2023-02-27 14:45:50 +0200

    minor changes to text format

--- manager/add_task.py
+++ manager/add_task.py
@@ -1,10 +1,10 @@
 from datetime import date, timedelta
 def add_task():
-    with open('current_cred.txt', 'r') as f:
+    with open('current_user.txt', 'r') as f:
         username = f.read()
     task_complete = "No"
-    task_title = input("Please enter the name of the task: ")
-    task_descrip = input("Please enter a description of the task:\n")
+    task_title = input("Please enter the name of the task: ").title()
+    task_descrip = input("Please enter a description of the task:\n").lower()
     task_assigned = date.today()
     task_due = task_assigned + timedelta(days=2)
 
